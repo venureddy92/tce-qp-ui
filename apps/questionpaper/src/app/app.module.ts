2655e1e5a86b0dfc099cee58da21bd79f66796a8
akash-dew <40857953+akash-dew@users.noreply.github.com>
2021-07-20 13:18:49 +0530

    done changes
    done changes

--- apps/questionpaper/src/app/app.module.ts
+++ apps/questionpaper/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {QuestionPaperModule} from '../../../../libs/question-paper/src/lib/question-paper.module'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,QuestionPaperModule],
+  imports: [BrowserModule,QuestionPaperModule,NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
